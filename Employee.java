c9ad66066840d9d59bd92f8c7206f0c54702ee29
gokul-2001 <79854227+gokul-2001@users.noreply.github.com>
2021-03-10 19:49:51 +0530

    Create Employee.java
    prints employee name and salary

---
+++ Employee.java
@@ -0,0 +1,17 @@
+class Employee{
+int x = 40000;
+int y = 30000; 
+int z = 20000;
+public static void main(String[] args){
+int a = 1994; 
+int b = 2000; 
+int c = 1999;
+Employee obj1 = new Employee();
+System.out.println("Name Address year of joining Salary");
+System.out.println("Robert WallsStreet "    +a+            +obj1.x);  
+System.out.println("Sam WallsStreet "       +b+             +obj1.y); 
+System.out.println("John WallsStreet "      +c+             +obj1.z);
+}
+}
+
+
