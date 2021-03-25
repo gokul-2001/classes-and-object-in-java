77dbbaea3d0447094e5e9230da651f7b7b66cbd5
gokul-2001 <79854227+gokul-2001@users.noreply.github.com>
2021-03-25 16:30:32 +0530

    Update Employee.java
    prints as table

--- Employee.java
+++ Employee.java
@@ -1,17 +1,11 @@
 class Employee{
-int x = 40000;
-int y = 30000; 
-int z = 20000;
 public static void main(String[] args){
-int a = 1994; 
-int b = 2000; 
-int c = 1999;
-Employee obj1 = new Employee();
-System.out.println("Name Address year of joining Salary");
-System.out.println("Robert WallsStreet "    +a+            +obj1.x);  
-System.out.println("Sam WallsStreet "       +b+             +obj1.y); 
-System.out.println("John WallsStreet "      +c+             +obj1.z);
+Employee obj1 =new Employee();
+System.out.println("Name    year of joining    Address");
+System.out.println("Robert   1994              64C-Wallstreet");
+System.out.println("sam      2000              68D-Wallstreet"); 
+System.out.println("john     1999              26B-Wallstreet"); 
 }
-}
+} 
 
 
